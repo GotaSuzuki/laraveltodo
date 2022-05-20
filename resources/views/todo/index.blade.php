aeb95746b415b6d2691a1a45edb63fbc39ac3826
GotaSuzuki <go.baseball.0408@icloud.com>
2022-05-20 19:01:39 +0900

    bootstrapでスタイルを変化させてみた

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -6,21 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
-    @foreach($items as $item)
-    <tr>
-        <input type="checkbox" name="check" id="">
-        <td><a href="{{ action('App\Http\Controllers\TodoController@detail', $item->id) }}" id='title'>{{$item->title}}</a></td>
-        <br>
-        <td><a href="{{ action('App\Http\Controllers\TodoController@delete', $item->id) }}" id="delete">削除する</a></td>
-        <td><a href="{{ action('App\Http\Controllers\TodoController@update', $item->id) }}" id="update">編集する</a></td>
-        <br>
-        <hr>
-    </tr>
-    @endforeach
+    <div class="container">
+        <div class="row align-items-start">
+            @foreach($items as $item)
+            <tr>
+                <div class="col">
+                    <input type="checkbox" name="check" id="">
+                </div>
+                <div class="col">
+                    <td><a href="{{ action('App\Http\Controllers\TodoController@detail', $item->id) }}" id='title'>{{$item->title}}</a></td>
+                </div>
+                <div class="col">
+                    <td><a href="{{ action('App\Http\Controllers\TodoController@delete', $item->id) }}" id="delete">削除する</a></td>
+                </div>
+                <div class="col">
+                    <td><a href="{{ action('App\Http\Controllers\TodoController@update', $item->id) }}" id="update">編集する</a></td>
+                </div>
+            </tr>
+            <hr><br>
+            @endforeach
+
+        </div>
+    </div>
 
     <table>
         <form action="/" method="POST">
